254a10dc89bb1f95a1a6e79fe078b74a5d2d5b2f
Shuntaro Shimizu <shuntaro_shimizu@r.recruit.co.jp>
2024-05-17 15:54:23 +0900

    rename

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ Bun.serve({
   fetch(req) {
     const url = new URL(req.url);
     if (url.pathname === "/form")
-      return new Response(Bun.file("./public/form.html"));
-    return new Response(Bun.file("./public/detail.html"));
+      return new Response(Bun.file("./docs/form/index.html"));
+    return new Response(Bun.file("./docs/detail/index.html"));
   },
 });
